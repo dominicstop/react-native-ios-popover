d515116c86f41436ab59b7c3092ca35b3834d26d
Dominic Go <dominicstop@gmail.com>
2020-11-24 12:32:06 +0800

    🚫 Removed stubs/dummy impl.

--- ios/IosPopover.m
+++ ios/IosPopover.m
@@ -1,9 +1,1 @@
 #import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(IosPopover, NSObject)
-
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-
-@end
