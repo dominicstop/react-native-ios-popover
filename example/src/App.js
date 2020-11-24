99c641577a85c34a41715db9acd54e98254c2235
Dominic Go <dominicstop@gmail.com>
2020-11-24 12:24:55 +0800

    🛠 Changed tsx to js

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,15 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import IosPopover from 'react-native-ios-popover';
+
+import { } from 'react-native-ios-popover';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    IosPopover.multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Result</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type IosPopoverType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { IosPopover } = NativeModules;
-
-export default IosPopover as IosPopoverType;
