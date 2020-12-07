fb2d2868b2f852404d890809eec312e33e715876
Dominic Go <dominicstop@gmail.com>
2020-12-07 19:30:40 +0800

    ⭐️ Example - Added `PopoverViewExample01`

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,34 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 
-import { } from 'react-native-ios-popover';
+import PopoverViewExample01 from './components/PopoverViewExample01';
+
+
+const examples = [
+  PopoverViewExample01
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Result</Text>
-    </View>
+    <SafeAreaView style={styles.rootContainer}>
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+        {examples.map((element, index) => 
+          React.createElement(element, { 
+            key  : `example-item-${index}`,
+            index: (index + 1),
+            // pass down props
+          })
+        )}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  scrollViewContainer: {
+    paddingBottom: 30,
   },
 });
---
+++ example/src/components/PopoverViewExample01.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { useCallback, useRef } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+import { PopoverView } from 'react-native-ios-popover';
+import { Button } from './Button';
+import { ExampleItemPopoverView } from './ExampleItemPopoverView';
+
+
+export default function PopoverViewExample01(props) {
+  const popoverRef = useRef();
+  const onPressButton = useCallback(() => {
+    popoverRef.current.setVisibility();
+  });
+
+  return (
+    <ExampleItemPopoverView
+      title={'PopoverViewExample01'}
+      subtitle={'Bare Min. Example'}
+      desc={'Basic `PopoverView` example'}
+      {...props}
+    >
+      <PopoverView
+        ref={popoverRef}
+        renderPopoverContent={() => (
+          <View style={{padding: 10}}>
+            <Text style={styles.popoverText}>
+              {'Popover Content'}
+            </Text>
+          </View>
+        )}
+      >
+        <Button onPress={onPressButton}/>
+      </PopoverView>
+    </ExampleItemPopoverView>
+  );
+};
+
+const styles = StyleSheet.create({
+  popoverText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
