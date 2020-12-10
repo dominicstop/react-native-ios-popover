1f4a5018481483e7fdd17614e3c86e782cee1815
Dominic Go <dominicstop@gmail.com>
2020-12-10 17:59:10 +0800

    🆕 Added `Enums` Export

---
+++ src/Enums.js
@@ -0,0 +1,15 @@
+
+
+export const ArrowDirections = {
+  up     : 'up',
+  down   : 'down',
+  left   : 'left',
+  right  : 'right',
+  any    : 'any',
+  unknown: 'unknown',
+};
+
+export const PopoverSize = {
+  INHERIT: 'INHERIT',
+  STRETCH: 'STRETCH',
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export * from './PopoverView';
+export * from './Enums';
