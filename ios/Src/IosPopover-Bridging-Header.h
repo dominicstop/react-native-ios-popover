eeb42f250ac922ee8caf4cfb1056745ec37f507d
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-03-11 23:02:44 +0800

    🆕 Add/Import `RNIUtilities`

--- ios/Src/IosPopover-Bridging-Header.h
+++ ios/Src/IosPopover-Bridging-Header.h
@@ -5,7 +5,9 @@
 #import <React/RCTBridge.h>
 #import <React/RCTView.h>
 #import <React/RCTTouchHandler.h>
+
 #import <React/RCTUIManager.h>
+#import <React/RCTUIManagerUtils.h>
 
 #import <React/RCTConvert.h>
 #import <React/RCTImageSource.h>
