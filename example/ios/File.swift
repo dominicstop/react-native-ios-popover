566671e9a0194778f75d1b39802b134c95c60dfa
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-03-13 04:16:21 +0800

    ✨ Example - Minor Changes
    Summary: Fix example project build-related issues.

--- example/ios/File.swift
+++ example/ios/File.swift
@@ -2,7 +2,7 @@
 //  File.swift
 //  IosPopoverExample
 //
-//  Created by Dominic Go on 2/13/22.
+//  Created by Dominic Go on 3/13/22.
 //
 
 import Foundation
--- ios/Src/IosPopover-Bridging-Header.h
+++ ios/Src/IosPopover-Bridging-Header.h
@@ -1,3 +1,4 @@
+
 #if DEBUG
 #import <React/RCTDevLoadingView.h>
 #endif
