ef79eac617b010074d965afc0575c0137868da3e
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-03-13 03:05:49 +0800

    🆕 Add `RNICleanable`

---
+++ ios/Src/ReactNative/RNIHelpers/RNICleanable.swift
@@ -0,0 +1,15 @@
+//
+//  RNICleanable.swift
+//  react-native-ios-popover
+//
+//  Created by Dominic Go on 3/13/22.
+//
+
+import Foundation
+
+
+protocol RNICleanable: AnyObject {
+  
+  func cleanup();
+  
+};
