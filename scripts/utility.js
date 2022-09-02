9828edade49654541f30c5a0014e5a04921da9a8
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-09-02 16:50:52 +0800

    💫 Update: Script - `utility`

--- scripts/utility.js
+++ scripts/utility.js
@@ -29,16 +29,24 @@ const SCRIPT_CHOICE_KEYS = {
   exampleLocalDepInstallHard: 'exampleLocalDepInstallHard',
 
   libraryLocalDepInstallSoft: 'libraryLocalDepInstallSoft',
+  libraryLocalDepInstallHard: 'libraryLocalDepInstallHard',
 
   localDepInstallSoft: 'localDepInstallSoft',
+  localDepInstallHard: 'localDepInstallHard',
 };
 
 const RADIO_CHOICES_TYPES = {
   choice: 'choice',
   spacer: 'spacer',
+  header: 'header',
 };
 
 const RADIO_CHOICES = [{
+  type: RADIO_CHOICES_TYPES.header,
+  title: 'Example-Related',
+  spaceAmount: 1,
+
+}, {
   title: "Example - Local Install - Dep. - Soft ",
   subtitle: "Install example package dependencies w/o resetting",
   type: RADIO_CHOICES_TYPES.choice,
@@ -55,13 +63,29 @@ const RADIO_CHOICES = [{
   spaceAmount: 1,
 
 }, {
+  type: RADIO_CHOICES_TYPES.header,
+  title: 'Library-Related',
+  spaceAmount: 1,
+
+}, {
   title: "Library - Local Install - Dep. - Soft ",
   subtitle: "Install library dev. dependencies w/o resetting",
   type: RADIO_CHOICES_TYPES.choice,
   key: SCRIPT_CHOICE_KEYS.libraryLocalDepInstallSoft,
 
 }, {
+  title: "Library - Local Install - Dep. - Hard ",
+  subtitle: "Hard Reset + Install library dev. dependencies",
+  type: RADIO_CHOICES_TYPES.choice,
+  key: SCRIPT_CHOICE_KEYS.libraryLocalDepInstallHard,
+
+}, {
   type: RADIO_CHOICES_TYPES.spacer,
+  spaceAmount: 1,
+
+}, {
+  type: RADIO_CHOICES_TYPES.header,
+  title: '"Example + Library"-Related',
   spaceAmount: 1,
 
 }, {
@@ -69,7 +93,25 @@ const RADIO_CHOICES = [{
   subtitle: "Install library dev. dep. + example dep. w/o resetting",
   type: RADIO_CHOICES_TYPES.choice,
   key: SCRIPT_CHOICE_KEYS.localDepInstallSoft,
+}, {
+  title: "Local Install - Dep. - Hard ",
+  subtitle: "Hard Reset + Install library dev. dep. + example dep.",
+  type: RADIO_CHOICES_TYPES.choice,
+  key: SCRIPT_CHOICE_KEYS.localDepInstallHard,
 }];
+
+const CONFIG_FLAGS = {
+  skipDeleteExampleXCWorkspace: true,
+  skipDeleteExampleNodeModules: true,
+  skipDeleteExampleLockFile: true,
+
+  skipDeleteLibraryNodeModules: true,
+  skipDeleteLibraryLockFile: true,
+};
+
+const SHARED = {
+  didTriggeRefreshLocalLibraries: false
+};
 
 // #endregion
 
@@ -165,6 +207,12 @@ class Scripts {
   };
 
   static async refreshLocalLibraries(listener){
+    // guard check
+    if(SHARED.didTriggeRefreshLocalLibraries){
+      listener({status: "local libraries already refreshed, skipping..."});
+      return;
+    };
+
     listener({status: "Refreshing local libraries...."});
 
     await Helpers.spawn(
@@ -176,7 +224,11 @@ class Scripts {
       `cd $REACT_NATIVE_IOS_UTILITIES_DIR && yarn run bob build`, 
       listener
     );
+
+    SHARED.didTriggeRefreshLocalLibraries = true;
   };
+
+  // ---------------------------------------
 
   static async exampleOpenXcode(listener){
     listener({status: "Opening example xcworkspace..."});
@@ -188,13 +240,11 @@ class Scripts {
   }; 
 
   static async exampleDeleteWorkspace(listener){
-    const skipDeletingWorkspace = true;
-
     const prefix = "cd example/ios";
 
     listener({status: "Begin deleting example ios workspace project..."});
-    listener({status: "Delete Pods -  Deleting 1 of 3..."});
 
+    listener({status: "Delete Pods -  Deleting 1 of 3..."});
     await Helpers.spawn(
       `${prefix} && rm -rfv ./Pods`, 
       listener
@@ -208,7 +258,7 @@ class Scripts {
 
     listener({status: "Delete Pods - Deleting 3 of 3..."});
 
-    if(skipDeletingWorkspace){
+    if(CONFIG_FLAGS.skipDeleteExampleXCWorkspace){
       listener({status: "Skip deleting: IosPopoverExample.xcworkspace"});
 
     } else {
@@ -222,14 +272,52 @@ class Scripts {
   };
 
   static async exampleDeleteNodeModules(listener){
-    listener({status: "Deleting example node_modules..."});
+    const prefix = 'cd example &&';
+
+    // guard check
+    if(CONFIG_FLAGS.skipDeleteExampleNodeModules){
+      listener({status: "Skip: Delete example node_modules..."});
+      return;
+    };
+
+    listener({status: "Deleting example node_modules - 1/2..."});
 
     await Helpers.spawn(
-      `cd example && rm -rfv ./node_modules`, 
+      `${prefix} rm -rfv ./node_modules`, 
       listener
     );
 
+    listener({status: "Deleting example node_modules - 2/2..."});
+    
+    if(CONFIG_FLAGS.skipDeleteLibraryLockFile){
+      listener({status: "Skip - delete example lockfile"});
+
+    } else {
+      await Helpers.spawn(
+        `${prefix} rm -rfv ./yarn.lock`, 
+        listener
+      );
+    };
+
     listener({status: "Example node_modules deleted."});
+  };
+
+  static async exampleResetNodeModules(listener){
+    // guard check
+    if(CONFIG_FLAGS.skipDeleteExampleNodeModules){
+      listener({status: "Skip: Reset example node_modules..."});
+      return;
+    };
+
+    await Scripts.exampleDeleteNodeModules(listener);
+
+    listener({status: "Resetting example node_modules..."});
+    await Helpers.spawn(
+      `cd example && yarn`, 
+      listener
+    );
+
+    listener({status: "Example node_modules reset."});
   };
 
   static async exampleRemoveDependencies(listener){
@@ -270,8 +358,9 @@ class Scripts {
   static async exampleInstallDepLocal(listener){
     const prefix = "cd example";
     
-    listener({status: "Installing example local dependencies..."});
+    await Scripts.refreshLocalLibraries(listener);
 
+    listener({status: "Installing example local dependencies..."});
     await Helpers.spawn(
       `echo 'Library - Local Install: react-native-ios-utilities' && echo "DIR: $REACT_NATIVE_IOS_UTILITIES_DIR"`, 
       listener
@@ -297,6 +386,7 @@ class Scripts {
     listener({status: "react-native-ios-utilities example install complete."});
   };
 
+  // Script Choice:
   // install example local dep. + pods w/o resetting
   static async exampleUseDepLocalSoft(listener){
     listener({status: "Installing example local dependencies + pods..."});
@@ -305,11 +395,13 @@ class Scripts {
     await Scripts.exampleInstallPods(listener);
   };
 
+  // Script Choice: exampleLocalDepInstallHard
   // nuke xcworkspace then install example local dep. + pods
   static async exampleUseDepLocalHard(listener){
     listener({status: "Resetting example project + install local dep + pod install..."});
     
     await Scripts.exampleNukeWorkspace(listener);
+    await Scripts.exampleResetNodeModules(listener);
     await Scripts.exampleInstallDepLocal(listener);
     await Scripts.exampleInstallPods(listener);
   };
@@ -331,9 +423,59 @@ class Scripts {
     await Scripts.exampleInstallPods(listener);
   };
 
-  static async libraryUseDepLocalSoft(listener){
-    listener({status: "Install library dep. locally..."});
+  // ---------------------------------------
 
+  static async libraryDeleteNodeModules(listener){
+    // guard check
+    if(CONFIG_FLAGS.skipDeleteLibraryNodeModules){
+      listener({status: "Skip: Delete library node_modules..."});
+      return;
+    };
+
+    listener({status: "Deleting library node_modules - 1/2..."});
+
+    await Helpers.spawn(
+      `rm -rfv ./node_modules`, 
+      listener
+    );
+
+    listener({status: "Deleting library node_modules - 2/2..."});
+    
+    if(CONFIG_FLAGS.skipDeleteLibraryLockFile){
+      listener({status: "Skip - delete library lockfile"});
+
+    } else {
+      await Helpers.spawn(
+        `rm -rfv ./yarn.lock`, 
+        listener
+      );
+    };
+
+    listener({status: "Library node_modules deleted."});
+  };
+
+  static async libraryResetNodeModules(listener){
+    // guard check
+    if(CONFIG_FLAGS.skipDeleteLibraryNodeModules){
+      listener({status: "Skip: Reset library node_modules..."});
+      return;
+    };
+
+    await Scripts.libraryDeleteNodeModules(listener);
+
+    listener({status: "Resetting library node_modules..."});
+    await Helpers.spawn(
+      `yarn install`, 
+      listener
+    );
+
+    listener({status: "Library node_modules reset."});
+  };
+
+  static async libraryUseDepLocalSoft(listener){
+    await Scripts.refreshLocalLibraries(listener);
+
+    listener({status: "Install library dep. locally..."});
     await Helpers.spawn(
       `echo 'Library - Local Install: react-native-ios-utilities' && echo "DIR: $REACT_NATIVE_IOS_UTILITIES_DIR"`, 
       listener
@@ -345,13 +487,36 @@ class Scripts {
     );
   };
 
-  // soft install dep. for lib. + example
+  static async libraryUseDepLocalHard(listener){
+    listener({status: "Hard reset library + install library dep. locally..."});
+
+    await Scripts.libraryResetNodeModules(listener);
+    await Scripts.libraryUseDepLocalSoft(listener);
+
+    await Helpers.spawn(
+      `yarn add --dev $REACT_NATIVE_IOS_UTILITIES_DIR`, 
+      listener
+    );
+  };
+
+  // ---------------------------------------
+
+  // soft install dependency for library + example
   static async useDependencyLocalSoft(listener){
     listener({status: "Installing library + example dependencies locally w/o resetting..."});
 
-    await Scripts.refreshLocalLibraries();
-    await Scripts.libraryUseDepLocalSoft();
-    await Scripts.exampleUseDepLocalSoft();
+    await Scripts.refreshLocalLibraries(listener);
+    await Scripts.libraryUseDepLocalSoft(listener);
+    await Scripts.exampleUseDepLocalSoft(listener);
+  };
+
+  // hard install dependency for library + example
+  static async useDependencyLocalHard(listener){
+    listener({status: "Reset lib + example, then install library + example dep. locally..."});
+
+    await Scripts.refreshLocalLibraries(listener);
+    await Scripts.exampleUseDepLocalHard(listener);
+    await Scripts.libraryUseDepLocalHard(listener);
   };
 };
 
@@ -364,17 +529,18 @@ class RootElements {
 
   static screen = blessed.screen({
     smartCSR: true,
-    title: 'utility scripts'
+    title: 'utility scripts',
   });
 
   static pageChooseScriptForm = blessed.form({
     parent: RootElements.screen,
     width: '100%',
-    keys: true,
     padding: {
       left: 1,
       right: 1,
     },
+    keys: true,
+    tags: true,
   });
 
   static pageScriptOutputBox = blessed.box({
@@ -387,10 +553,13 @@ class RootElements {
     scrollable: true,
     alwaysScroll: true,
     mouse: true,
+    tags: true,
   });
 };
 
 class ChooseScriptPage {
+  static enableDebug = false;
+
   static radioSetScriptChoice = blessed.radioset({
     parent: RootElements.pageChooseScriptForm,
     width: '100%',
@@ -398,6 +567,10 @@ class ChooseScriptPage {
   });
 
   static getSelectedScriptChoiceKey(data = {}, choices = []){
+    const choicesFiltered = RADIO_CHOICES.filter(item =>
+      item.type === RADIO_CHOICES_TYPES.choice
+    );
+
     // { scriptChoice: [ true, false, false, false ] }
     const scriptChoiceRaw = (data ?? {})[ID_KEYS.scriptChoice] ?? [];
 
@@ -411,7 +584,7 @@ class ChooseScriptPage {
 
     const scriptChoiceArray = (() => {
       const dataCount   = scriptChoiceArrayRaw.length;
-      const targetCount = choices.length;
+      const targetCount = choicesFiltered.length;
       const diffCount   = targetCount - dataCount;
 
       if(diffCount <= 0){
@@ -420,7 +593,7 @@ class ChooseScriptPage {
       } else {
         let pre = [];
 
-        for (let i = 0; i < (diffCount - 2); i++) {
+        for (let i = 0; i < (diffCount); i++) {
           pre.push(false);
         };
 
@@ -428,10 +601,6 @@ class ChooseScriptPage {
       };
       
     })();
-
-    const choicesFiltered = RADIO_CHOICES.filter(item =>
-      item.type === RADIO_CHOICES_TYPES.choice
-    );
 
     // i.e. `[false, false, true...]`
     const selectedIndex = scriptChoiceArray.findIndex(item => item);
@@ -441,13 +610,15 @@ class ChooseScriptPage {
       : choicesFiltered[selectedIndex]?.key ?? null
     );
 
-    (false && console.log('\n DEBUG', 
+    (ChooseScriptPage.enableDebug && console.log(
+      '\n DEBUG', 
       '\n - scriptChoiceRaw:', scriptChoiceRaw,  
       '\n - scriptChoiceArrayRaw', scriptChoiceArrayRaw, 
       '\n - scriptChoiceArray', scriptChoiceArray,
       `\n - selectedIndex: ${selectedIndex}`,
-      `\n - choices[selectedIndex]: ${JSON.stringify(choices[selectedIndex])}`,
-      `\n - choiceKey: ${choiceKey}`
+      `\n - choicesFiltered[selectedIndex]: ${JSON.stringify(choicesFiltered[selectedIndex])}`,
+      `\n - choiceKey: ${choiceKey}`,
+      `\n\n - data:\n${JSON.stringify(data)}`,
     ));
 
     return choiceKey;
@@ -493,6 +664,22 @@ class ChooseScriptPage {
           const spaceAmount = item.spaceAmount ?? 1;
           topCounter +=spaceAmount;
           break;
+
+        case RADIO_CHOICES_TYPES.header:
+          blessed.text({
+            parent: radioSetScriptChoice,
+            content: item.title,
+
+            top: topCounter++,
+            left: 0,
+            style: {
+              underline: true,
+              bold: true,
+            },
+          });
+
+          topCounter += item.spaceAmount ?? 0;
+          break;
       
         default:
           break;
@@ -505,6 +692,8 @@ class ChooseScriptPage {
         ChooseScriptPage.getSelectedScriptChoiceKey(data, choicesFiltered);
 
       if(selectedScriptChoiceKey != null){
+        if(ChooseScriptPage.enableDebug) return;
+
         // A - Item was selected...
         // "navigate" to `scriptOutput` page
         RootElements.pageChooseScriptForm.destroy();
@@ -518,6 +707,8 @@ class ChooseScriptPage {
 
       } else {
         // B - Nothing was selected...
+        if(!ChooseScriptPage.enableDebug) return;
+
         RootElements.screen.destroy();
         process.exit(1);
       };
@@ -640,6 +831,8 @@ class ScriptOutputPage {
           ScriptOutputPage.log(output);
         });
         break;
+      
+      // ------------------------------
 
       case SCRIPT_CHOICE_KEYS.libraryLocalDepInstallSoft:
         await Scripts.libraryUseDepLocalSoft((output) => {
@@ -647,13 +840,31 @@ class ScriptOutputPage {
         });
         break;
 
+      case SCRIPT_CHOICE_KEYS.libraryLocalDepInstallHard:
+        await Scripts.libraryUseDepLocalHard((output) => {
+          ScriptOutputPage.log(output);
+        });
+        break;
+
+      // ------------------------------
+
       case SCRIPT_CHOICE_KEYS.localDepInstallSoft:
         await Scripts.useDependencyLocalSoft((output) => {
           ScriptOutputPage.log(output);
         });
         break;
 
+      case SCRIPT_CHOICE_KEYS.localDepInstallHard:
+        await Scripts.useDependencyLocalHard((output) => {
+          ScriptOutputPage.log(output);
+        });
+        break;
+
+      // ------------------------------
+      
       case 'test':
+        ScriptOutputPage.logStatus('Choice not implemented, running test script instead...');
+
         // debug
         await Scripts.testHelloWorld(({status, stdout, stderr}) => {
           if(status){
@@ -681,8 +892,7 @@ class ScriptOutputPage {
     };
 
     ScriptOutputPage.logStatus('Script Finished - Press Q to quit');
-    ScriptOutputPage.logStatus('Bye b\n');
-    topStatus.scroll(Number.MAX_VALUE);
+    ScriptOutputPage.logStatus('Bye b xx\n');
   };
 };
 
