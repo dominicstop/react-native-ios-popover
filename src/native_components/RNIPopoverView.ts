46eaf1ecc82c00eb79fff4c3664eead0cbdaec74
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2022-10-28 16:37:23 +0800

    💄 Gloss: Update Comments

--- src/native_components/RNIPopoverView.ts
+++ src/native_components/RNIPopoverView.ts
@@ -1,12 +1,18 @@
 import { requireNativeComponent, Platform, ViewProps, HostComponent, View } from 'react-native';
+import type { RNICleanupMode } from 'react-native-ios-utilities';
 
 import type { PopoverArrowDirections, PopoverSizeConfig } from '../types/PopoverRelatedTypes';
 import type { OnPopoverDidHideEvent, OnPopoverWillShowEvent, OnPopoverWillHideEvent, OnPopoverDidShowEvent, OnPopoverDidHideViaTapEvent, OnPopoverWillHideViaTapEvent, OnPopoverDidAttemptToDismissEvent } from '../types/PopoverViewEvents';
 
 // TODO: Add type annotation - Remove `any` type usage
 export type RNIPopoverViewProps = ViewProps & {
+  // Internal
+  // --------
 
   internalCleanupMode?: RNICleanupMode;
+
+  // Value Props
+  // -----------
 
   popoverBackgroundColor?: number;
   permittedArrowDirections?: Array<PopoverArrowDirections>;
@@ -14,6 +20,9 @@ export type RNIPopoverViewProps = ViewProps & {
 
   popoverShouldDismiss?: boolean;
   popoverCanOverlapSourceViewRect?: boolean;
+
+  // Events - Lifecycle
+  // ------------------
 
   onPopoverDidHide?: OnPopoverDidHideEvent;
   onPopoverWillShow?: OnPopoverWillShowEvent;
